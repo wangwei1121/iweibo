70eade8fa58effc299b1f223a5f68c68a5d4fba6
wang.weib <wang.weib@ctrip.com>
2016-01-29 15:15:04 +0800

    微博认证授权

---
+++ app/src/main/java/com/snail/iweibo/oauth/Constants.java
@@ -0,0 +1,13 @@
+package com.snail.iweibo.oauth;
+
+/**
+ * Created by wang.weib on 2016/1/29.
+ */
+public interface Constants {
+    public static final String APP_KEY = "2160936024";           // 应用的APP_KEY
+    public static final String REDIRECT_URL = "http://www.sina.com";// 应用的回调页
+    public static final String SCOPE = "email,direct_messages_read,direct_messages_write,"
+                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+                    + "follow_app_official_microblog," + "invitation_write";
+
+}
---
+++ app/src/main/java/com/snail/iweibo/ui/activity/WBAuthActivity.java
@@ -0,0 +1,155 @@
+package com.snail.iweibo.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
+import com.snail.iweibo.R;
+import com.snail.iweibo.oauth.AccessTokenKeeper;
+import com.snail.iweibo.oauth.Constants;
+
+import java.text.SimpleDateFormat;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class WBAuthActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Bind(R.id.obtain_token_via_web)Button button;
+
+    /** 显示认证后的信息，如 AccessToken */
+    @Bind(R.id.token_text_view) TextView mTokenText;
+
+    private AuthInfo mAuthInfo;
+
+    /** 封装了 "access_token"，"expires_in"，"refresh_token"，并提供了他们的管理功能  */
+    private Oauth2AccessToken mAccessToken;
+
+    /** 注意：SsoHandler 仅当 SDK 支持 SSO 时有效 */
+    private SsoHandler mSsoHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wbauth);
+        ButterKnife.bind(this);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.obtain_token_via_web:
+                mAuthInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
+                mSsoHandler = new SsoHandler(WBAuthActivity.this, mAuthInfo);
+                mSsoHandler.authorizeWeb(new AuthListener());
+                break;
+        }
+    }
+
+    /**
+     * 当 SSO 授权 Activity 退出时，该函数被调用。
+     *
+     * @see {@link Activity#onActivityResult}
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // SSO 授权回调
+        // 重要：发起 SSO 登陆的 Activity 必须重写 onActivityResults
+        if (null != mSsoHandler) {
+            mSsoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+
+    }
+
+    class AuthListener implements WeiboAuthListener {
+
+        @Override
+        public void onComplete(Bundle values) {
+            // 从 Bundle 中解析 Token
+            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+            //从这里获取用户输入的 电话号码信息
+            String  phoneNum =  mAccessToken.getPhoneNum();
+            if (mAccessToken.isSessionValid()) {
+                // 显示 Token
+                updateTokenView(false);
+
+                // 保存 Token 到 SharedPreferences
+                AccessTokenKeeper.writeAccessToken(WBAuthActivity.this, mAccessToken);
+                Toast.makeText(WBAuthActivity.this,
+                        R.string.weibosdk_demo_toast_auth_success, Toast.LENGTH_SHORT).show();
+            } else {
+                // 以下几种情况，您会收到 Code：
+                // 1. 当您未在平台上注册的应用程序的包名与签名时；
+                // 2. 当您注册的应用程序包名与签名不正确时；
+                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+                String code = values.getString("code");
+                String message = getString(R.string.weibosdk_demo_toast_auth_failed);
+                if (!TextUtils.isEmpty(code)) {
+                    message = message + "\nObtained the code: " + code;
+                }
+                Toast.makeText(WBAuthActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(WBAuthActivity.this,
+                    R.string.weibosdk_demo_toast_auth_canceled, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            Toast.makeText(WBAuthActivity.this,
+                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 显示当前 Token 信息。
+     *
+     * @param hasExisted 配置文件中是否已存在 token 信息并且合法
+     */
+    private void updateTokenView(boolean hasExisted) {
+        String date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(
+                new java.util.Date(mAccessToken.getExpiresTime()));
+        String format = getString(R.string.weibosdk_demo_token_to_string_format_1);
+        mTokenText.setText(String.format(format, mAccessToken.getToken(), date));
+
+        String message = String.format(format, mAccessToken.getToken(), date);
+        if (hasExisted) {
+            message = getString(R.string.weibosdk_demo_token_has_existed) + "\n" + message;
+        }
+        mTokenText.setText(message);
+    }
+
+}
