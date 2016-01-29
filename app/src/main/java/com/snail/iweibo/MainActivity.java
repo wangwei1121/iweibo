a0b6c746275232f7b602ab528cb6d6a3062319ff
wang.weib <wang.weib@ctrip.com>
2016-01-29 10:54:05 +0800

    测试修改

--- app/src/main/java/com/snail/iweibo/MainActivity.java
+++ app/src/main/java/com/snail/iweibo/MainActivity.java
@@ -1,37 +1,55 @@
 package com.snail.iweibo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button)this.findViewById(R.id.button_1);
+        button1.setOnClickListener(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.button_1:
+                Toast.makeText(this, "activity 跳转", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this,ToolbarActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
