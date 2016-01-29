eed377d56486297716db85bb56118cec6f967fe1
alexwan1989 <alexwan1989@sina.com>
2016-01-29 09:43:54 +0800

    test

--- app/src/main/java/com/snail/iweibo/ui/activity/MainActivity.java
+++ app/src/main/java/com/snail/iweibo/ui/activity/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends BaseAppCompatActivity<IMainActivityView> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
     }
 
