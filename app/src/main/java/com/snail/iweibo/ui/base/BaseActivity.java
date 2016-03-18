d2eeb291e82c9ad2fbd825a48a42d705ec16fd2d
wang.weib <wang.weib@ctrip.com>
2016-03-18 17:38:00 +0800

    首页修改

---
+++ app/src/main/java/com/snail/iweibo/ui/base/BaseActivity.java
@@ -0,0 +1,56 @@
+package com.snail.iweibo.ui.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.snail.iweibo.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by wang.weib on 2016/3/18.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
