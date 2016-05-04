6c01fa3d3bc3f6c7b9d37b5a20dd46aed24350b4
wangwei <137736102@qq.com>
2016-05-04 23:45:29 +0800

    修改BaseAppliaction

--- app/src/main/java/com/snail/iweibo/util/BaseApplication.java
+++ app/src/main/java/com/snail/iweibo/util/BaseApplication.java
@@ -12,6 +12,7 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         context = getApplicationContext();
     }
     public static Context getContext(){
